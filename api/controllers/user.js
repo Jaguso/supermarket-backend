90070bbdac812af32b9c89fc4d2b54aed205e666
Jaguso <jagutzo@gmail.com>
2019-05-22 20:28:38 -0500

    made email unique on user register

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,24 +5,45 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 exports.user_register = (req, res, next) => {
-    
-    const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    });
-    user.save()
-        .then(result => {
-            console.log(result)
-            res.status(201).json({
-                message: 'User cratead successfully'
-            })
+
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: 'Email already exists'
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            username: req.body.username,
+                            email: req.body.email,
+                            password: hash
+                        });  
+                        user.save()
+                            .then(result => {
+                                console.log(result)
+                                res.status(201).json({
+                                    message: 'User cratead successfully'
+                                })
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                });
+                            }); 
+                    }
+                })
+            }
         })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
+    
+    
+    
 }
