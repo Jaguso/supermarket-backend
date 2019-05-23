1e4ba30f9f94586ff86132a3843bdd8af6906aea
Jaguso <jagutzo@gmail.com>
2019-05-22 19:44:38 -0500

    connected to mongo data base

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true }
+)
 
 app.get('/', (req, res) => {
     res.send("it woorks")
