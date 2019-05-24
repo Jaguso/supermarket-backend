27be77983bb6192c583008ef23aa3122ae40b9ea
Jaguso <jagutzo@gmail.com>
2019-05-23 21:47:30 -0500

    made controller to create a list

---
+++ api/controllers/list.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const List = require('../models/list');
+
+exports.list_create = (req, res, next) => {
+    const list = new List({
+        _id: new mongoose.Types.ObjectId(),
+        title: req.body.title,
+        elements: req.body.elements
+    });
+    list.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                response
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+}
